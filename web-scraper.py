3e47c42cb40d19f736bc04f5f7b6416ae04594f4
Jakob L. Truong <jakobtruong@Jakobs-M1-MacBook-Pro.local>
2024-03-31 21:40:32 -0700

    Added Selenium configuration options and got chrome webdriver running

--- web-scraper.py
+++ web-scraper.py
@@ -1,8 +1,17 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+website_to_scrape = 'http://books.toscrape.com/'
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--incognito')
+options.add_argument('--headless')
+driver = webdriver.Chrome()
+driver.get(website_to_scrape)
+
 def main():
-  pass
+    while True:
+      pass
 
 if __name__ == "__main__":
   main()
